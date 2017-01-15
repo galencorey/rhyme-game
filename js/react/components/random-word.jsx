a3e6ff627a2041f8164c17824aeaacdeaa31c0cd
Galen Corey <galen.corey@gmail.com>
2017-01-15 08:32:13 -0500

    centered div on page

--- js/react/components/random-word.jsx
+++ js/react/components/random-word.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {fetchWord} from '../../redux/action-creators';
 import RaisedButton from 'material-ui/RaisedButton';
-import {textStyles} from '../styles';
+import {textStyles, centeredDiv} from '../styles';
 
 const RandomWord = ({fetchWord, word}) => {
-  return(<div>
+  return(<div style={centeredDiv}>
           { word? null: <h2 style={textStyles}>Are you a rhyme master?</h2>}
-          <RaisedButton label="Play" primary={true} onClick={fetchWord} />
-          <p style={textStyles} >{word}</p>
+          <RaisedButton label={word? "New Word": "Play"} primary={true} onClick={fetchWord} />
+          <h3 style={textStyles}>{word}</h3>
         </div>)
 }
 
--- js/react/styles.js
+++ js/react/styles.js
@@ -1,3 +1,10 @@
 export const textStyles = {
   fontFamily: 'Roboto, sans-serif'
 }
+
+export const centeredDiv = {
+  width: "50%",
+  margin: "auto",
+  textAlign: "center",
+  paddingTop: 15
+}
