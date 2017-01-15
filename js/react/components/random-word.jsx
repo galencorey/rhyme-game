de5b658b23256f58570755051155013f9b85014e
Galen Corey <galen.corey@gmail.com>
2017-01-15 08:23:14 -0500

    added roboto font

--- js/react/components/random-word.jsx
+++ js/react/components/random-word.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {fetchWord} from '../../redux/action-creators';
 import RaisedButton from 'material-ui/RaisedButton';
+import {textStyles} from '../styles';
 
 const RandomWord = ({fetchWord, word}) => {
   return(<div>
+          { word? null: <h2 style={textStyles}>Are you a rhyme master?</h2>}
           <RaisedButton label="Play" primary={true} onClick={fetchWord} />
-          <p>{word}</p>
+          <p style={textStyles} >{word}</p>
         </div>)
 }
 
---
+++ js/react/styles.js
@@ -0,0 +1,3 @@
+export const textStyles = {
+  fontFamily: 'Roboto, sans-serif'
+}
