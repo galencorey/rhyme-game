d9c3eca2d93d629bc0e0646dfe1e0ab7bf59298f
Galen Corey <galen.corey@gmail.com>
2017-01-17 11:48:11 -0500

    logging from server

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const {resolve} = require('path')
 const app = express()
 
+console.log("API key from server", process.env.wordsAPIKey)
+
 console.log("filepath", resolve(__dirname, 'index.html'))
 
 app.use(express.static('js'));
